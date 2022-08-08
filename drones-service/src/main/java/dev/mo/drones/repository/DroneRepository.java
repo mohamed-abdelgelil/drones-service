972f693ee9c0f88d734a57123a8eb1ddd2e8a220
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-08-08 11:38:51 +0200

    Build the Drone Repository
    - findDroneById
    - findAvailableDrones in ('IDLE','RETURNING') state
    - findLowBattartLevels (below 25)

--- drones-service/src/main/java/dev/mo/drones/repository/DroneRepository.java
+++ drones-service/src/main/java/dev/mo/drones/repository/DroneRepository.java
@@ -1,5 +1,23 @@
 package dev.mo.drones.repository;
 
-public interface DroneRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import dev.mo.drones.model.Drone;
+
+
+@Repository
+public interface DroneRepository extends JpaRepository<Drone, Long> {
+	
+	@Query("SELECT d FROM Drone d WHERE d.id = ?1")
+	Drone findDroneById(Long droneID);
+	
+	@Query("SELECT d FROM Drone d WHERE d.state IN ('IDLE','RETURNING')")
+	List<Drone> findAvailableDrones();
+	
+	@Query("SELECT d FROM Drone d WHERE d.btCapacity < 25")
+	List<Drone> findLowBattartLevels();
 
 }
