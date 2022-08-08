519a1f903e530c089400e0cc6aa8e180ef524361
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-08-08 12:27:58 +0200

    Build the Drone Controller Layer
    - add Drone Service
    - Get Available Drones
    - Get Drone Battery

--- drones-service/src/main/java/dev/mo/drones/controller/DroneController.java
+++ drones-service/src/main/java/dev/mo/drones/controller/DroneController.java
@@ -1,5 +1,37 @@
 package dev.mo.drones.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.mo.drones.dto.DroneDTO;
+import dev.mo.drones.service.DroneService;
+
+@RestController
+@RequestMapping("/drone")
 public class DroneController {
+
+	@Autowired
+	DroneService droneService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST, headers = "Accept=application/json")
+	public DroneDTO addDrone(@RequestBody DroneDTO droneDTO) {
+		return droneService.addDrone(droneDTO);
+	}
+
+	@RequestMapping(value = "/get", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<DroneDTO> getAvailableDrone() {
+		return droneService.getAvailableDrones();
+	}
+
+	@RequestMapping(value = "/{droneID}/battary/get", method = RequestMethod.GET, headers = "Accept=application/json")
+	public int getDroneBattary(@PathVariable Long droneID) {
+		return droneService.getBattaryLevel(droneID);
+	}
 
 }
