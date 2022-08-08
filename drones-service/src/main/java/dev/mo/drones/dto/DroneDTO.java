3c3896692a82a5e569c6f3456c63ec9d241dc3a9
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-08-08 11:29:51 +0200

    Implement The DroneDTO & MedicationDTO
    - Build the Data Transfer Objects to be used at the exposed and integration service to:
       - hide the entities details
       - to be able to customize at the DTOs without affecting the entities

--- drones-service/src/main/java/dev/mo/drones/dto/DroneDTO.java
+++ drones-service/src/main/java/dev/mo/drones/dto/DroneDTO.java
@@ -1,5 +1,62 @@
 package dev.mo.drones.dto;
 
-public class DroneDTO {
+import java.io.Serializable;
+
+public class DroneDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7789945395635464977L;
+
+	private String serial;
+
+	private String model;
+
+	private int weight;
+
+	private int btCapacity;
+
+	private String state;
+
+	public String getSerial() {
+		return serial;
+	}
+
+	public void setSerial(String serial) {
+		this.serial = serial;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public int getBtCapacity() {
+		return btCapacity;
+	}
+
+	public void setBtCapacity(int btCapacity) {
+		this.btCapacity = btCapacity;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 
 }
