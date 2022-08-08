14ac0cb322cb8e564f95d2dc233f65d2a1012bd5
Mohamed.Abdelgelil <mohamed.abdelgelil@qsitint.com>
2022-08-08 10:22:55 +0200

    Spring-Boot Initial Commit
    Creating the Spring-boot project with the defaults

---
+++ drones-service/src/main/java/com/musala/drones/DronesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musala.drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesServiceApplication.class, args);
+	}
+
+}
