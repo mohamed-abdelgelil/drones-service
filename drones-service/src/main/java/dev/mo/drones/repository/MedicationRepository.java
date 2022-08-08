05b0bb26b8768e8f7068e3f46b7ef404d864e954
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-08-08 11:39:33 +0200

    Implement Medication Repo.
    - findMedicationsByDroneID

--- drones-service/src/main/java/dev/mo/drones/repository/MedicationRepository.java
+++ drones-service/src/main/java/dev/mo/drones/repository/MedicationRepository.java
@@ -1,5 +1,18 @@
 package dev.mo.drones.repository;
 
-public interface MedicationRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import dev.mo.drones.model.Medication;
+
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
+	@Query("SELECT M FROM Medication M WHERE M.drone.id = ?1")
+	List<Medication> findMedicationsByDroneID(Long droneID);
 
 }
