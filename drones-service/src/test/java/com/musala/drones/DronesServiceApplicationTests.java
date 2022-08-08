d50c2f8ca152fc8138fe6ddd60922c4e206edda0
Mohamed.Abdelgelil <m.abdelgelil.abdo@gmail.com>
2022-08-08 14:36:14 +0200

    Update DronesServiceApplicationTests.java

--- drones-service/src/test/java/com/musala/drones/DronesServiceApplicationTests.java
+++ drones-service/src/test/java/com/musala/drones/DronesServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.musala.drones;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import dev.mo.drones.DronesServiceApplication;
+import dev.mo.drones.dto.DroneDTO;
+import dev.mo.drones.dto.MedicationDTO;
+import dev.mo.drones.service.DroneService;
+import dev.mo.drones.service.MedicationService;
+
+@SpringBootTest(classes = {DronesServiceApplication.class})
 class DronesServiceApplicationTests {
 
+	@Autowired 
+	DroneService droneService;
+	
+	@Autowired
+	MedicationService medicationService;
+	
 	@Test
-	void contextLoads() {
+	void addDrone() {
+		DroneDTO droneDTO =new DroneDTO();
+		droneDTO.setSerial("123456789");
+		droneDTO.setModel("Lightweight");
+		droneDTO = droneService.addDrone(droneDTO);
+	}
+	
+	@Test
+	void LoadMedication() {
+		MedicationDTO medicationDTO1 = new MedicationDTO();
+		medicationDTO1.setCode("ss150");
+		medicationDTO1.setName("Puls");
+		medicationDTO1.setWeight(100);
+		medicationService.addMedicationToDrone(medicationDTO1, 1l);
 	}
 
 }
